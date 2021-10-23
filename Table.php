0d737e4b4715969a87b6efddea5fd423bb7f9218
Sukma Abdi Prakasa <sukmaabdiprakasa@yahoo.co.id>
2021-10-23 20:40:54 +0700

    create table

---
+++ Table.php
@@ -0,0 +1,85 @@
+<html lang="en">
+    <head>
+    <title>cuti</title>
+    </head>
+
+<body>
+    <H1> Lorem Ipsum </H1>
+    <h2> Lorem Ipsum </H2>
+    <h3> Lorem Ipsum </H3>
+    <h4> Lorem Ipsum </H4>
+    <h5> Lorem Ipsum </H5>
+    <h6> Lorem Ipsum </H6>
+    <!--  ini header ukuran tex h1 - h6-->
+    <hr>
+    <a href="index.php">Index</a> |
+    <a href="table.php">Table</a> |
+    <a href="table.php">form</a>  |
+    <hr>
+    <!-- buat tabel -->
+    
+
+    <!-- <table></table>
+        <tr> </tr>
+        <th> </th>
+        <td> </td>
+        
+     -->
+     <h3> Table Normal</h3>
+     <table border="1" width="50%">
+     
+     <br>
+         <tr>
+            <th align="center" valign="">a</th>
+            <th>b</th>
+            <th>c</th>
+         </tr>
+         </table>
+
+
+         <h3>Table colpsan</h3>
+
+<table border="1" width="50%">
+         <tr>
+             <th colspan="2" >ab</th>
+             
+             <th>c</th>
+             
+         </tr>
+         
+         
+         <tr>
+             <td>d</td>
+             <td colspan=3>ef</td>
+             
+         </tr>
+         <tr>
+             <td>g</td>
+             <td>h</td>
+             <td>i</td>
+         </tr>
+         </table>     
+         
+         <h3> Table rowspan</h3>
+     <table border="1" width="50%">
+     
+     <br>
+         <tr>
+            <th align="center" valign="" rowspan="2">a</th>
+            <th>b</th>
+            <th>c</th>
+         </tr>
+
+         <tr>
+          
+            <th>e</th>
+            <th>f</th>
+         </tr>
+         <tr>
+            <th align="center" valign="">g</th>
+            <th>h</th>
+            <th>i</th>
+         </tr>
+         </table>
+     
+</html>
